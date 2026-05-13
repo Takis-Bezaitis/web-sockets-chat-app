6422929511bc14e1f25f11a3371600a24c331113
takis85 <t.mpezaitis@ccseducation.com>
2026-05-13 11:18:37 +0300

    fix: comment out MobileNavBar import and conditional rendering in Chat component

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -7,7 +7,7 @@ import { useWebRTCStore } from "../store/webrtcStore";
 
 import ChatSidebar from "../components/chat/ChatSidebar";
 import UsersInRoom from "../components/chat/UsersInRoom";
-import MobileNavBar from "../components/layout/MobileNavBar";
+//import MobileNavBar from "../components/layout/MobileNavBar";
 import VideoCallWindow from "../components/video/VideoCallWindow";
 import ChatContent from "../components/chat/ChatContent";
 import IncomingCallModal from "../components/video/IncomingCallModal";
@@ -29,7 +29,7 @@ const Chat = () => {
 
   useChatSockets(currentRoom?.id);
 
-  const { mobileView, videoOverlay, showMembers, setMobileView, setVideoOverlay, setShowMembers, 
+  const { mobileView, videoOverlay, showMembers, setMobileView, setShowMembers, 
     inCall, callState, isCaller } = useChatLayout();
 
   const { typingUserByRoom } = useTypingStore();
@@ -278,13 +278,13 @@ const Chat = () => {
         </div>
       </div>
       {/* ------- MOBILE NAV BAR (bottom) ------- */}
-      {!isDesktop && (
+      {/*!isDesktop && (
         <div className="fixed bottom-0 left-0 right-0 z-50 bg-background">
           <MobileNavBar 
           mobileView={mobileView} setMobileView={setMobileView} 
           videoOverlay={videoOverlay} setVideoOverlay={setVideoOverlay} />
         </div>
-      )}
+      )*/}
     </>
   );
 };
